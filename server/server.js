b3c72d6dbeb5770002b4983f5b0672e5bffa1387
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2024-12-15 15:08:40 +0100

    created backend

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.json())
+
+app.get('/', express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`)});
