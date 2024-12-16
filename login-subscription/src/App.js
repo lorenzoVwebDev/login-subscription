43711f1686cb66c7aa3b0603936cb69d212f90b8
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2024-12-16 19:24:18 +0100

    started practice from scratch

--- login-subscription/src/App.js
+++ login-subscription/src/App.js
@@ -1,28 +1,6 @@
-import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard/Dashboard.js'
-import Preferences from './components/Preferences/Preferences.js'
-import Login from './components/Login/Login.js'
-
 function App() {
-  const [token, setToken] = useState();
-   
-  if (!token) {
-    return <Login setToken={setToken}/>;
-  }
 
-  return (
-    <div>
-      <h1>Application</h1>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/preferences" element={<Preferences/>}/>
-      </Routes>
-    </BrowserRouter>
-    </div>
-  );
 }
 
 export default App;
