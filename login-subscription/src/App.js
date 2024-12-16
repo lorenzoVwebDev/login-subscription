1ef80fdebe5c247647b6322b92eab77bf07520aa
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2024-12-16 18:50:14 +0100

    updated readme

--- login-subscription/src/App.js
+++ login-subscription/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { Browser, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard.js'
 import Preferences from './components/Preferences/Preferences.js'
 
 function App() {
   return (
-    <Dashboard/>
+    <div>
+      <h1>Login</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/preferences' element={<Preferences/>}/>
+      </Routes>
+    </BrowserRouter>
+    </div>
   );
 }
 
