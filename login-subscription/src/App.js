f616cdcd19d4508d31e4c39d00c7d0f1f5beda41
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2024-12-16 19:14:45 +0100

    created login component

--- login-subscription/src/App.js
+++ login-subscription/src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard.js'
 import Preferences from './components/Preferences/Preferences.js'
+import Login from './components/Login/Login.js'
 
 function App() {
   return (
     <div>
-      <h1>Login</h1>
+      <h1>Application</h1>
     <BrowserRouter>
       <Routes>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/preferences' element={<Preferences/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/preferences" element={<Preferences/>}/>
       </Routes>
     </BrowserRouter>
     </div>
