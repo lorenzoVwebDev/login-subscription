ccb45ddbfc13d3cab6011bc206cfc602978c5713
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2024-12-27 19:07:15 +0100

    updated

--- login/src/components/App/App.js
+++ login/src/components/App/App.js
@@ -5,15 +5,33 @@ import Dashboard from '../Dashboard/Dashboard.js';
 import Preferences from '../Preferences/Preferences.js';
 import Login from '../Login/Login.js';
 import { useToken } from '../services/token.js';
-import loginImage from '../../images/loginImage.png'
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles({
+  link: {
+    color: 'white',
+    fontSize: '2.1vw',
+    borderStyle: 'solid'
+  }
+})
+/* const useStyles = createUseStyles({
+  link: {
+    color: 'white',
+    fontSize: '3.1vw',
+    borderStyle: 'solid'
+  }
+}) */
 
 function App() {
+  const classes = useStyles();
   const { setToken, token } = useToken()
   const [ authok, setAuthok] = useState(true)
   if (!token) {
     return (
       <>
-      <header></header>
+          <header className="header-2">
+          git Repository 👉🏻 <a href="https://github.com/lorenzoVwebDev/loginPage">Login page</a>
+          </header>
       <div className="main-container">
       <div className="login-img-container">
         <img className="login-img"src="https://i.ibb.co/88LGhZh/login-Image.png" alt="" />
@@ -29,12 +47,16 @@ function App() {
   }
   return (
     <>
-    <header>
-    git Repository 👉🏻 <a href="https://github.com/lorenzoVwebDev/Animal-Cards">Login page</a>
+    <header className="header-1">
+    git Repository 👉🏻 <a href="https://github.com/lorenzoVwebDev/loginPage">Login page</a>
     </header>
     <div className="application-wrapper">
       <div className="applications-wrapper">
-      <h1>Application</h1>
+      <h1>You have logged into my project!</h1>
+      <h1>Just delete the token to try the other credentials.</h1>
+      <h2>&#40;I am even creating the subscription page so that</h2>
+      <h2>you'll be able to create your own credentials!&#41;</h2>
+      <h3><a href="https://github.com/lorenzoVwebDev/loginPage" className={classes.link}> Click here</a> to know more about my login page</h3>
       <BrowserRouter>
         <Link to="dashboard"><li>Dashboard</li></Link>
         <Link to="preferences"><li>Preferences</li></Link>
